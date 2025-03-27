b6f00082ffde9dbeada93db681013af3ea6c4dba
Hanna Shymanovych <shimsnov111@gmail.com>
2025-03-28 00:24:23 +0100

    Complete project with final functionality and design

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <main>
+    <img className='react-logo' src="/src/assets/react.svg"  alt="Light blue atomic-style symbol with three intersecting elliptical orbits around a central blue circle, representing the interconnected nature of React's component-based architecture" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
 )
