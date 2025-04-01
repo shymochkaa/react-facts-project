d540884b8565ea088690698aa46d156c863f35b4
Hanna Shymanovych <shimsnov111@gmail.com>
2025-04-01 16:50:27 +0200

    Update the project structure and design

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img className="react-logo" src="src/assets/react-logo.png"/>
+                <a href="https://react.dev">ReactFacts</a>
+    
+            </nav>
+        </header>
+        
+    )
+
+}
